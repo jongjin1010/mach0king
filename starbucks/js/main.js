61843bda78e7b97fb37d134dab2a88b0fbb6d120
jongjin1010 <jongjin024@gmail.com>
2022-06-05 04:36:47 +0900

    순차적으로 요소 보이기

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -31,3 +31,12 @@ window.addEventListener('scroll', _.throttle(function () {
   }
 }, 300));
 // _.throttle(함수, 시간)
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
+});
